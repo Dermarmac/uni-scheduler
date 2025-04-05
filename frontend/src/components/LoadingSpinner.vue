79beb5e7e51471d91304c6eb9c23e7c1104b3509
dermarmc <dermar.mcfarlane@gmail.com>
2025-04-05 17:26:59 -0400

    add spinner loader

---
+++ frontend/src/components/LoadingSpinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="text-center pa-10">
+    <v-progress-circular indeterminate color="primary" size="48" />
+    <p class="mt-4">Loading component...</p>
+  </v-container>
+</template>
+
+<script setup></script>
+
+<style scoped>
+p {
+  font-size: 14px;
+  color: #888;
+}
+</style>
